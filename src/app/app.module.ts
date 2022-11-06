2c603e7012b06f9da545d22a42291f85474a0a94
Andrés Cendales <andres01660@gmail.com>
2022-11-06 12:26:28 -0500

    feat: adding modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {CafeModule} from "./cafe/cafe.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CafeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
