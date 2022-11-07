e779482e36c8be0fd324856247cc7d79e0325dd8
Andrés Cendales <andres01660@gmail.com>
2022-11-06 19:28:39 -0500

    feat: adding total by cafe's type

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -10,6 +10,8 @@ import {CafeService} from "../cafe.service";
 export class CafeListComponent implements OnInit {
 
   cafes: Array<Cafe> = [];
+  cafesOrigen: number = 0;
+  cafesBlend: number = 0;
 
   constructor(private cafeService:CafeService) {}
 
@@ -17,6 +19,8 @@ export class CafeListComponent implements OnInit {
     this.cafeService.getCafes().subscribe(
         (cafes: Cafe[]) => {
         this.cafes = cafes;
+        this.cafesOrigen = cafes.filter(cafe => cafe.tipo != "Blend").length;
+        this.cafesBlend = cafes.filter(cafe => cafe.tipo == "Blend").length;
       }
     );
   }
